d6daad047cd296b1d3528a64c881cb273fc63c8f
onkarsherkar <onkar.sherkar@gmail.com>
2021-12-04 21:03:43 +0530

    Signup process added

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Note
+from werkzeug.security import generate_password_hash, check_password_hash
+from website import db
 
 auth = Blueprint('auth', __name__)
 
@@ -18,20 +21,19 @@ def signup():
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
-    flag = True
     if len(email) < 3:
-      flag = False
       flash('Email minimum have 4 letter', category='error')
-    if len(firstName) < 3:
-      flag = False
+    elif len(firstName) < 3:
       flash('First name minimum have 4 letter', category='error')
-    if len(password1) < 3:
-      flag = False
+    elif len(password1) < 3:
       flash('Password minimum have 4 letter', category='error')
-    if password1 != password2:
-      flag = False
+    elif password1 != password2:
       flash('Password is not matching', category='error')
-    if flag:
+    else:
+      new_user = User(email=email, password=generate_password_hash(password1, method='sha256'), first_name=firstName)
+      db.session.add(new_user)
+      db.session.commit()
       flash('Account is created!', category='success')
+      return redirect(url_for('views.home'))
 
   return render_template("sign_up.html")
