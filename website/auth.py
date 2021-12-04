0cc213077abe55dbf030392378b15690d2fcf947
onkarsherkar <onkar.sherkar@gmail.com>
2021-12-04 16:30:13 +0530

    flash messages added

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
   return render_template("login.html")
 
@@ -10,6 +10,17 @@ def login():
 def logout():
   return render_template("home.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if password1 != password2:
+      flash('Password is not matching', category='error')
+    else:
+      flash('Account is created!', category='success')
+
   return render_template("sign_up.html")
